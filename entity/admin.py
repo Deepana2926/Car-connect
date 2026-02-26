3fe195f8199f144828669e18fc539c0e46d92e38
Deepana <156581227+Deepana2926@users.noreply.github.com>
2026-02-26 14:30:23 +0530

    Add files via upload

---
+++ entity/admin.py
@@ -0,0 +1,11 @@
+class Admin:
+    def __init__(self, admin_id, first_name, last_name, email, phone, username, password, role, created_date):
+        self.admin_id = admin_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.phone = phone
+        self.username = username
+        self.password = password
+        self.role = role
+        self.created_date = created_date
---
+++ entity/vehicle.py
@@ -0,0 +1,9 @@
+class Vehicle:
+    def __init__(self, vehicle_id, model, make, year, color, registration_number, availability):
+        self.vehicle_id = vehicle_id
+        self.model = model
+        self.make = make
+        self.year = year
+        self.color = color
+        self.registration_number = registration_number
+        self.availability = availability
